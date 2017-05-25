9d694a4492cbde40469b26915606b73511ba5e2d
Tedy Tri Saputro <tedy.saputro@ruangtedy.net>
2017-05-26 04:52:47 +0700

    first commit

---
+++ src/main/java/com/embeddednesia/web/controllers/HomeController.java
@@ -0,0 +1,27 @@
+package com.embeddednesia.web.controllers;
+
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+	private static final Logger logger=LoggerFactory.getLogger(HomeController.class);
+	
+	
+	
+	@RequestMapping(method = RequestMethod.GET)
+	public String home(Locale locale, Model model){
+		logger.info("Welcome to Taskify! The client locale is {}.", locale);
+		
+		return "home";
+		
+	}
+	
+}
